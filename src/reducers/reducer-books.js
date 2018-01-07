381a5ee34754250617f998801efa756a98783690
Burhan Ali <burhan.ali@graze.com>
2018-01-07 23:13:44 +0000

    Add a book list reducer

---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'World War Z' },
+        { title: 'The Handmaid\'s Tale' },
+        { title: '1984' },
+        { title: 'Code Complete' }
+    ];
+}
