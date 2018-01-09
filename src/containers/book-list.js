7ba615327e7a6dd1f791ebed8b4e950d1bde4b7b
Burhan Ali <burhan.ali@graze.com>
2018-01-09 21:53:14 +0000

    Connect up container and state

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className={list-group-item}>{book.title}</li>
+                <li key={book.title} className="list-group-item">{book.title}</li>
             );
         });
     }
     render() {
         return (
-            <ul className="lisy-group col-sm-4">
+            <ul className="list-group col-sm-4">
                 {this.renderList()}
             </ul>
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
