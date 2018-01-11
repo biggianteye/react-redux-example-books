bcfc4972257dfeea2dbe5d92075a40545968acbc
Burhan Ali <burhan.ali@graze.com>
2018-01-11 21:30:11 +0000

    Handle empty initial state and display some properties

--- src/reducers/reducer-books.js
+++ src/reducers/reducer-books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'World War Z' },
-        { title: 'The Handmaid\'s Tale' },
-        { title: '1984' },
-        { title: 'Code Complete' }
+        { title: 'World War Z', pages: 100 },
+        { title: 'The Handmaid\'s Tale', pages: 202 },
+        { title: '1984', pages: 157 },
+        { title: 'Code Complete', pages: 2007 }
     ];
 }
